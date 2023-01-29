0f49402bca90dfaf75ea84411103fce796b556bb
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2023-01-29 19:51:35 +0800

    About version

---
+++ docs/version.c
@@ -0,0 +1,13 @@
+// Platfer - Philippines’ first web browser
+// Copyright 2022-2023 Cyril John Magayaga
+// About version
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Platfer – Philippines' first web browser\n");
+    printf("v2.0-preview3 | January 31, 2023\n");
+    printf("\n");
+    printf("Copyright 2022-2023 Cyril John Magayaga");
+    return 0;
+}
